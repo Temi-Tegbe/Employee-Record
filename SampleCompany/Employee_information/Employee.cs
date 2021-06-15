0426be21c2c7d9dd2af6b310ab165428c795d12f
Temi-Tegbe <66542303+Temi-Tegbe@users.noreply.github.com>
2021-06-15 10:22:21 +0100

    Add files via upload

---
+++ SampleCompany/Employee_information/Employee.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCompany.Employee_information
+{
+    class Employee
+    {
+        private readonly int _staffId;
+        private readonly string _fullName;
+        private readonly string _employmentDate;
+        private string _jobTitle;
+        private string _department;
+        public Employee(int staffId, string fullName, string employmentDate)
+        {
+            _staffId = staffId;
+            _fullName = fullName;
+            _employmentDate = employmentDate;
+        }
+
+        public int staffId
+        {
+            get => _staffId;
+        }
+
+        public string fullName
+        {
+            get => _fullName;
+        }
+
+        public string employmentDate
+        {
+            get => _employmentDate;
+        }
+
+        public string JobTitle
+
+        {
+            get => _jobTitle;
+
+            set { _jobTitle = value; }
+        }
+
+        public string Department
+
+        {
+            get => _department;
+            set { _department = value; }
+
+        }
+
+        public void ClockIn()
+        {
+            Console.WriteLine("I am clocking in");
+        }
+
+        public void ClockOut()
+        {
+            Console.WriteLine("I am clocking out");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("I am currently working");
+        }
+    }
+}
---
+++ SampleCompany/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using SampleCompany.Employee_information;
+
+
+namespace SampleCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee firstEmployee = new Employee(1001, "John Doe", "12/12/2020");
+            Employee secondEmployee = new Employee(1002, "Jane Doe", "13/12/2020");
+            Employee thirdEmployee = new Employee(1003, "John Thomas", "14/12/2020");
+            Employee fourthEmployee = new Employee(1004, "outOf Names", "15/12/2020");
+            Employee fifthEmployee = new Employee(1005, "outOf Names2", "16/12/2020");
+
+
+            firstEmployee.JobTitle = "Frontend Developer";
+            secondEmployee.JobTitle = "Backend Developer";
+            thirdEmployee.JobTitle = "Head HR";
+            fourthEmployee.JobTitle = "Business Analyst";
+            fifthEmployee.JobTitle = "DevOps Engineer";
+
+            firstEmployee.Department = "Engineering";
+            secondEmployee.Department = "Engineering";
+            thirdEmployee.Department = "Human Resiurces";
+            fourthEmployee.Department = "Business Development";
+            fifthEmployee.Department = "Engineering";
+
+            Employee[] employeeArray = new Employee[5] { firstEmployee, secondEmployee, thirdEmployee, fourthEmployee, fifthEmployee };
+
+
+
+            foreach (Employee i in employeeArray)
+            {
+                Console.WriteLine(i.staffId + "\t\t" + i.fullName + "\t" + i.employmentDate + "\t" + i.Department + "\t\t" + i.JobTitle + "\n");
+                
+            }
+        }
+    }
+}
+
+            
+
+        
+
+
+
